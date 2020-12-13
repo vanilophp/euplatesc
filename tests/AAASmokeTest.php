a3a78c6d69bce950571bad5a750f907aed610b69
Attila Fulop <fulopattila122@gmail.com>
2020-12-13 10:16:36 +0100

    CS fixes (PSR-12)

--- tests/AAASmokeTest.php
+++ tests/AAASmokeTest.php
@@ -13,7 +13,7 @@ namespace Konekt\Euplatesc\Tests;
 
 class AAASmokeTest extends TestCase
 {
-    const MIN_PHP_VERSION = '7.3.0';
+    public const MIN_PHP_VERSION = '7.3.0';
 
     /**
      * @test
@@ -31,8 +31,10 @@ class AAASmokeTest extends TestCase
      */
     public function php_version_satisfies_requirements()
     {
-        $this->assertFalse(version_compare(PHP_VERSION, self::MIN_PHP_VERSION, '<'),
+        $this->assertFalse(
+            version_compare(PHP_VERSION, self::MIN_PHP_VERSION, '<'),
             'PHP version ' . self::MIN_PHP_VERSION . ' or greater is required but only '
-            . PHP_VERSION . ' found.');
+            . PHP_VERSION . ' found.'
+        );
     }
 }
